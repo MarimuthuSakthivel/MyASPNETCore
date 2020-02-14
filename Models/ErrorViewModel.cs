b2824a4589dbf3bd0fe8b26d981b7f28db0e90eb
Marimuthu <maris@prophoenix.com>
2020-02-14 14:04:29 +0530

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MyASPNETCore.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
